c90241f49a7b95cb783c410cfb7999a992880afe
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-19 22:16:47 +0100

    Fatto esercizio normale e bonus

---
+++ Snack Pomeriggio - 1/Bonus - ciclo while e output in pagina semplice/js/script.js
@@ -0,0 +1,40 @@
+// CONSEGNA
+
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo.
+
+
+// SOLUZIONE
+// Variabile del numero inserito (gli do stringa vuota di default per farlo entrare nel while)
+let userNumber = '';
+while(userNumber === ''){
+
+    // Ricevo l'input dall'utente
+    userNumber = prompt(`Inserisci un numero`);
+
+    // Output solo se è un numero valido e l'utente non ha annullato
+    if(!(userNumber === '') && !(userNumber === null)){
+
+        // Variabile per stringa convertita in numero
+        const userRealNumber = parseInt(userNumber);
+        
+        // Stampo il numero se è pari altrimenti stampo il numero + 1
+        if(userRealNumber % 2 === 0){
+            document.querySelector('body').innerHTML = `
+            <h2 class="text-dark text-center mt-5">${userRealNumber} è pari, quindi stampo ${userRealNumber}</h2>
+            `
+        }
+        else{
+            document.querySelector('body').innerHTML = `
+            <h2 class="text-dark text-center mt-5">${userRealNumber} è dispari, quindi stampo ${userRealNumber + 1}</h2>
+            `
+        }
+    } else if(userNumber === ''){
+        alert(`Per favore inserisci un numero valido`);
+    } else{
+        alert(`Hai annullato l'operazione`);
+    }      
+}
+
+
+ 
--- Snack Pomeriggio - 1/js/script.js
+++
@@ -1,7 +0,0 @@
-const userNumber = parseInt(prompt(`Dammi un numero`));
-
-if(userNumber % 2 === 0){
-    console.log(userNumber);
-} else{
-    console.log(userNumber +1);
-}
