32ec6b8e4eaec5cab93506187a03fd1fc9da84f8
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-20 11:29:36 +0100

    Finito snack 4 Normale

---
+++ Snack Pomeriggio - 4/Normale/js/script.js
@@ -0,0 +1,43 @@
+// CONSEGNA 
+
+// Crea un array vuoto e chiedi all’utente un numero
+// da inserire nell’array. Continua a chiedere i numeri
+// all’utente e a inserirli nell’array fino a quando
+// la somma degli elementi è minore di 50.
+ 
+
+// SOLUZIONE
+
+// Creo l'array vuoto
+const numbersArray = [];
+// Creo la variabile somma
+let sum = 0;
+// Creo la variabile per il numero che darà l'uscita dal while (mi serve per l'output)
+let errorNumber;
+
+// Chiedo i numeri all'utente finché la somma è minore di 50
+while(sum < 50){
+    // Chiedo il numero all'utente
+    const currentNumber = parseInt(prompt(`Inserisci un numero`));
+    // Aumento la somma prima di pushare nell'array così l'ultimo numero in input,
+    // quello che farebbe superare la somma di 50, non verrà aggiunto.
+    // Se l'utente digita 40, 9 e 10 aggiungo alla lista solo 40 e 9 
+    // poiché quando digita 10 la somma supera 49 e quel 10 non va aggiunto all'array
+    sum += currentNumber;
+
+    // Lo aggiungo all'array solo se la somma è ancora minore di 50 dopo l'ultimo incremento
+    // Se la somma supera 50 registro l'ultimo numero come numero che fa chiudere il while
+    if(sum < 50){
+        numbersArray.push(currentNumber);
+    } else{
+        errorNumber = currentNumber;
+    } 
+}
+
+alert(`
+Prima che la somma fosse >= 50, hai digitato questi numeri: ${numbersArray}
+
+La somma era arrivata a ${sum - errorNumber}
+
+Con l'ultimo numero che hai inserito (${errorNumber}) la somma è arrivata a: ${sum}
+`);
